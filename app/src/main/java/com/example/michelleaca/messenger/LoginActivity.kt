9996da1676baf8b1f9f50e035f088c74214e1de6
MichAca <m.aca@outlook.ph>
2018-07-26 12:05:57 +0800

    log in initial commit

---
+++ app/src/main/java/com/example/michelleaca/messenger/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.michelleaca.messenger
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
